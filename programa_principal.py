473fd952e8abcf973d710055da5db5093112d57f
martinasergi <msergiirazoqui@udesa.edu.ar>
2026-03-13 17:46:48 -0300

    Create programa_principal.py

---
+++ programa_principal.py
@@ -0,0 +1,5 @@
+import funciones_habitos
+lista = funciones_habitos.registrar_habitos()
+resultado = funciones_habitos.analizar_habitos(lista)
+print(f"Resumen de actividades: {lista}")
+print(resultado)
